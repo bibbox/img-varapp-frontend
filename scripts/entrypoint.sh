508ae97478f9a5fe999819d70acb27a7c4d734d1
Emilian <emilian.jung+github@gmail.com>
2022-03-17 13:26:53 +0100

    new env variables in frontend

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
 
-
-backend_url=${BACKEND_URL:-"'+window.location.protocol + '\/\/' + window.location.hostname+':8000\/varapp'+'"}
-sed -i "s/http:\/\/localhost:8000\/varapp/$backend_url/g" /var/www/html/varapp-web/conf/conf.js
+#backend_protocol=${BACKEND_PROTOCOL:-"'+window.location.protocol+'"}
+backend_protocol="'+window.location.protocol+'"
+if [[ ! -z "${BACKEND_PROTOCOL}" ]]; then
+  backend_protocol="${BACKEND_PROTOCOL}:"
+fi
+#backend_url=${BACKEND_LOCATION:-"'+window.location.protocol + '\/\/' + window.location.hostname+':8000\/varapp'+'"}
+backend_location=${BACKEND_LOCATION:-"'+ window.location.hostname+':8000\/varapp'+'"}
+backend_url="${backend_protocol}\/\/${backend_location}"
+sed -i -e "s@http:\/\/localhost:8000\/varapp@${backend_url}@g" /var/www/html/varapp-web/conf/conf.js
 
 service apache2 start 
 
